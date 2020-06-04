341094dd31778848ff3cdb81c4d3a56ef8e58b5f
Roberto Junior <robertojr-13-@hotmail.com>
2020-06-04 05:51:26 -0300

    Add Index in the PointsController

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -6,6 +6,19 @@ import knex from '../database/connection';
 class PointsController {
   async index(request: Request, response: Response) {
     const { city, uf, items } = request.query;
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
   }
 
   async show(request: Request, response: Response) {
@@ -39,7 +52,8 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image:
+        'https://images.unsplash.com/photo-1533900298318-6b8da08a523e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
