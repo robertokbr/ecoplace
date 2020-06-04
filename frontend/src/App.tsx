ddd48fe90d7dd6519abecd7941b6bd1225f9bcda
Roberto Junior <robertojr-13-@hotmail.com>
2020-06-04 19:47:21 -0300

    Create Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>HELLO NEXT LEVEL!!</h1>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <Routes />;
+};
 
 export default App;
---
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,129 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      <form>
+        <h1>
+          Cadastro do
+          <br />
+          ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="Whatsapp">Whatsapp</label>
+              <input type="text" name="Whatsapp" id="Whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/eletronicos.svg"
+                alt="produto"
+              />
+              <span>Óleo de cozinha</span>
+            </li>
+          </ul>
+        </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
+    </div>
+  );
+};
+export default CreatePoint;
