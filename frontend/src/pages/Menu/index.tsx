0826592459d61a285400b67e00b9991b2254ca2e
Roberto Junior <robertojr-13-@hotmail.com>
2020-08-05 21:16:56 -0300

    Add page Menu

--- frontend/src/pages/Menu/index.tsx
+++ frontend/src/pages/Menu/index.tsx
@@ -4,6 +4,9 @@ import { withTheme } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Container, Card, Cards, Header } from './styles';
 import ecoIcon from '../../assets/icons/ecoIcon.svg';
+import iconBuy from '../../assets/icons/iconBuy.svg';
+import iconPoints from '../../assets/icons/iconPoints.svg';
+import iconMyAnnounce from '../../assets/icons/iconMyAnnounce.svg';
 
 const Menu: React.FC = () => {
   return (
@@ -17,42 +20,48 @@ const Menu: React.FC = () => {
       <Cards>
         <Card>
           <Link to="/create-announce">
-            <img src={ecoIcon} alt="Eco" />
+            <img src={iconBuy} alt="Eco" />
 
             <div>
               <strong>Ecommerce</strong>
               <p>Crie seus próprios anuncios de produtos ecologicos</p>
-              <FiArrowRight size={20} color="white" />
+              <FiArrowRight size={30} />
             </div>
           </Link>
         </Card>
         <Card>
-          <Link to="/create-announce">
+          <Link to="/create-point">
+            <img src={ecoIcon} alt="Eco" />
+
             <div>
               <strong>Ecoleta</strong>
               <p>Anuncie um ponto de coleta de reciclaveis</p>
-              <FiArrowRight size={20} color="white" />
+              <FiArrowRight size={30} />
             </div>
           </Link>
         </Card>
         <Card>
-          <Link to="/create-announce">
+          <Link to="/map">
+            <img src={iconPoints} alt="Eco" />
+
             <div>
               <strong>Buscar</strong>
               <p>
                 Busque por pontos de coleta e anúncios de produtos ecologicos
               </p>
-              <FiArrowRight size={20} color="white" />
+              <FiArrowRight size={30} />
             </div>
           </Link>
         </Card>
         <Card>
-          <Link to="/create-announce">
+          <Link to="/get-point">
+            <img src={iconMyAnnounce} alt="Eco" />
+
             <div>
               <strong>Seus anuncios</strong>
               <p>Reveja seus anúncios</p>
 
-              <FiArrowRight size={20} color="white" />
+              <FiArrowRight size={30} />
             </div>
           </Link>
         </Card>
