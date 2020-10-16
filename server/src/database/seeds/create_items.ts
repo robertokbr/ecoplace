a70e9791e988ee1fee091efa5d67f0db63fc850e
robertokbr <robertojuniordev@gmail.com>
2020-10-16 12:55:36 -0300

    Alter database seed

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -5,7 +5,7 @@ export async function seed(knex: Knex) {
     { title: 'Comércio', image: 'kart.svg' },
     { title: 'Lãmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Papelão e garrafas', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
     { title: 'Óleo de Cozinha', image: 'oleo.svg' },
